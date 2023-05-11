e891a20ef6b9a8635c6db19626146abeb91ef7fb
wjpearson <willjamespearson@gmail.com>
2023-05-11 15:16:51 +0200

    tile number in catalogues

--- scripts/catalogue_scripts/make_post_catalogue_script.py
+++ scripts/catalogue_scripts/make_post_catalogue_script.py
@@ -4,13 +4,13 @@ sys.path.append('/home/pearsonw/dev/XID_plus/') #CHANGE ME!
 import xidplus
 from xidplus import extra_functions as extra
 
-output_folder = '/home/pearsonw/dev/XID_plus/output/'
+output_folder = '/home/pearsonw/dev/XID_plus/output_numpyro/'
 tile_file_name = 'MIPS_24_vanilla_'
 Master_filename = 'Master_prior_vanilla.pkl'
 instrument = 'MIPS'     #SPIRE or PACS or MIPS
 
-start_tile = 0
-end_tile = 1000
+start_tile = 1250
+end_tile = 1270
 
 if instrument == 'SPIRE':
     extra.make_master_SPIRE_post_catalogue_HEALpix(output_folder, Master_filename, tile_file_name, start_tile=start_tile, end_tile=end_tile)
